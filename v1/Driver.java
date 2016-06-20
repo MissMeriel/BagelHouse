08cd6db640a6d80826243f0d7cc27682e6297c1d
Meriel Stein <meriel.k.stein@gmail.com>
2016-06-20 10:18:02 -0500

    The rest of the folder

---
+++ v1/Driver.java
@@ -0,0 +1,15 @@
+package v1;
+
+
+/**
+ *  This program creates an instance of the OrderCalculatorGUI
+ *  class, which displays the GUI for Della's House of Bagels
+ */
+
+public class Driver
+{
+   public static void main(String[] args)
+   {
+      new OrderCalculatorGUI();
+   }
+}
